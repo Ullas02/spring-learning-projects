a8f1bace668f919151b2ae4a0d95c278b5c874ce
Ullas02 <166708206+Ullas02@users.noreply.github.com>
2026-07-05 16:08:58 +0530

    Update Car.java

--- spring-core/spring-core-day-02-first-project/Car.java
+++ spring-core/spring-core-day-02-first-project/Car.java
@@ -1,5 +1,13 @@
 public class Car {
 
-    private Engine engine = new Engine();
+    private Engine engine;
 
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void drive() {
+        engine.start();
+        System.out.println("Car is moving...");
+    }
 }
