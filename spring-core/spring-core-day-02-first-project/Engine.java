f9c26286a065790175357e521b49fab74c279573
Ullas02 <166708206+Ullas02@users.noreply.github.com>
2026-07-05 15:53:18 +0530

    Add Car class with drive method using Engine

---
+++ spring-core/spring-core-day-02-first-project/Engine.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    private Engine engine;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void drive() {
+        engine.start();
+    }
+}
