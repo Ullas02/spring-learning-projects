c5e65fd559d319003e3dd5dd958ae21412bf1038
Ullas <ullas.s.1729@gmail.com>
2026-07-16 14:38:35 +0530

    Started with Spring Boot learning

---
+++ spring-boot/spring-boot-day-01-first-project/src/main/java/com/example/demo/SpringBootDay01FirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDay01FirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDay01FirstProjectApplication.class, args);
+    }
+
+}
